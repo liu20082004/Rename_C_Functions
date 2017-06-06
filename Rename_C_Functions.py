9ff239a8eada0a35746ef89c1e1069630a8c3c64
hongyuliu <912901426@qq.com>
2017-06-06 18:11:05 +0800

    fit PEP8

--- Rename_C_Functions.py
+++ Rename_C_Functions.py
@@ -9,23 +9,25 @@ import shutil
 def version():
 	return 'V2.2'
 
+
 # 找到指定目录下特点后缀的文件
-def findFileByTypes(path, isfullroot, *filetypes):
-	result = []
-	for root, dirs, files in os.walk(path):
+def find_file_by_types(root, isfullroot, *filetypes):
+	tab_of_files = []
+	for root, dirs, files in os.walk(root):
 		for f in files:
 			if os.path.isfile(os.path.join(root, f)) and os.path.splitext(f)[1][1:] in filetypes:  # os.path.join(root,f) 合成一个路径
-				if False == isfullroot:
-					result.append(os.path.join(root, f)[len(path) + 1:])
+				if not isfullroot:
+					tab_of_files.append(os.path.join(root, f)[len(root) + 1:])
 				else:
-					result.append(os.path.join(root, f))
+					tab_of_files.append(os.path.join(root, f))
 				# print os.path.join(root,f)
-	return result
+	return tab_of_files
 
-def find_all_C_functions(projectfiles):
+
+def find_all_c_functions(projectfiles):
 	#
 	functions = []
-	functionsRule = re.compile('^(?!if\b|else\b|while\b|[\s\*])(?:[\w\*~_&]+?\s+){1,6}([\w:\*~_&]+\s*)\([^\);]*\)[^\{;]*?(?:^[^\r\n\{]*;?[\s]+){0,10}\{', re.M)  # 正则表达式设置:
+	functions_rule = re.compile('^(?!if\b|else\b|while\b|[\s\*])(?:[\w\*~_&]+?\s+){1,6}([\w:\*~_&]+\s*)\([^\);]*\)[^\{;]*?(?:^[^\r\n\{]*;?[\s]+){0,10}\{', re.M)  # 正则表达式设置:
 
 	try:
 		for file_path in projectfiles:
@@ -33,69 +35,66 @@ def find_all_C_functions(projectfiles):
 			data = targetfile.read()
 			targetfile.close()
 
-			found_functions = re.findall(functionsRule, data)
+			found_functions = re.findall(functions_rule, data)
 			for found_function in found_functions:
 				if found_function not in functions:
 					functions.append(found_function)
-		return [0,functions]
+		return [0, functions]
 	except IOError, e:
 		return [1, e]
 
-def rename_C_Functions(str_C_file):
-	try:
-		c_file = open(str_C_file, 'r')
-		data_org = c_file.readlines()
-		c_file.close()
-	except IOError, e:
-		print e
-		return [1, e]
 
-def get_Data_From_File(path, filename):
-	filepath = os.path.join(path, filename)  # 合成全路径
+def get_data_from_file(root, filename):
+	filepath = os.path.join(root, filename)  # 合成全路径
 	try:
 		c_file = open(filepath, 'r')
 		data = c_file.readlines()
 		c_file.close()
 	except IOError, e:
-		return [1,e]
+		return [1, e]
 	else:
 		newdata = []
 		for item in data:
-			newdata.append(item.strip()) # 删除行尾换行符
+			newdata.append(item.strip())  # 删除行尾换行符
 		return [0, newdata]
 
-def	 get_Global(path):
-	filename = 'global.ini'
-	return get_Data_From_File(path, filename)
 
-def get_Static(path):
+def get_global(root):
+	filename = 'global.ini'
+	return get_data_from_file(root, filename)
+
+
+def get_static_from_ini(root):
 	filename = 'static.ini'
-	[errorCode, data] = get_Data_From_File(path, filename)
+	[error_code, data] = get_data_from_file(root, filename)
 	newdata = []
 	for item in data:
 		newdata.append(item.split('\t'))
-	return [errorCode, newdata]
+	return [error_code, newdata]
 
-def get_Function(path):
+
+def get_function_from_ini(root):
 	filename = 'function.ini'
-	return get_Data_From_File(path, filename)
+	return get_data_from_file(root, filename)
 
-def change_init(path):
-	[errorCode, globalData] = get_Global(path)
-	if 1 == errorCode :
-		return [errorCode, globalData]
 
-	[errorCode, staticData] = get_Static(path)
-	if 1 == errorCode :
-		return [errorCode, staticData]
+def change_init(root):
+	[error_code, global_list] = get_global(root)
+	if 1 == error_code:
+		return [error_code, global_list]
 
-	[errorCode, functionData] = get_Function(path)
-	if 1 == errorCode :
-		return [errorCode, functionData]
+	[error_code, static_list] = get_static_from_ini(root)
+	if 1 == error_code:
+		return [error_code, static_list]
 
-	return [errorCode, globalData, staticData, functionData]
+	[error_code, function_list] = get_function_from_ini(root)
+	if 1 == error_code:
+		return [error_code, function_list]
 
-def add_NULL_For_Free(path,data):
+	return [error_code, global_list, static_list, function_list]
+
+
+def add_null_for_free(root, data):
 
 	free_func_tab = [
 		['init_vci_config_lib.c','free(g_p_stVciParamsGroup[i]);','{\n\t\t\t\tfree(g_p_stVciParamsGroup[i]);\n\t\t\t\tg_p_stVciParamsGroup[i]=NULL;\n\t\t\t}'],
@@ -127,78 +126,78 @@ def add_NULL_For_Free(path,data):
 
 	newdata = data
 	for item in free_func_tab:
-		if (item[1] in data) and (item[0] in path):
+		if (item[1] in data) and (item[0] in root):
 			newdata = item[2]
 	return newdata
 
+
 def remove_the_same_prefix(inlist):
 	inlist.sort(key=lambda x: len(x), reverse=False)
-	lenOfInlist = len(inlist)
-	newList = inlist[:]
-	for i in range(0,lenOfInlist):
+	len_of_inlist = len(inlist)
+	new_list = inlist[:]
+	for i in range(0, len_of_inlist):
 		for item in inlist[i:]:
 			if (inlist[i] != item) and (0 == item.find(inlist[i])):
-				newList.remove(item)
-	return newList
+				new_list.remove(item)
+	return new_list
 
-def add_getversion_api(path):
+
+def add_getversion_api(root):
 	str_cfile = '\\interface\\protocol_interface.c'
 	str_hfile = '\\interface\\protocol_interface.h'
 	str_c_getversion = '\n//获取库的版本\n\nint get_version()\n{\n\treturn Version;\n}\n'
 	str_h_getversion = '\t//获取库的版本\n\tPROTOCOL_API int get_version();\n\n#undef PROTOCOL_API'
 
 	try:
-		cfile = open(path+str_cfile,'r')
+		cfile = open(root + str_cfile, 'r')
 		org_data = cfile.read()
 		cfile.close()
 
 		if -1 != org_data.find('#define Version 0000.000.0002'):
-			org_data = org_data.replace('#define Version 0000.000.0002','#define Version 00000000')
+			org_data = org_data.replace('#define Version 0000.000.0002', '#define Version 00000000')
 
 		if -1 == org_data.find('int get_version()'):
-			org_data = org_data+ str_c_getversion
+			org_data = org_data + str_c_getversion
 
-		cfile = open(path+str_cfile,'w')
+		cfile = open(root + str_cfile, 'w')
 		cfile.write(org_data)
 		cfile.close()
 
-	except IOError,e:
-		return [1,e]
+	except IOError, e:
+		return [1, e]
 
 	try:
-		hfile = open(path+str_hfile,'r')
+		hfile = open(root + str_hfile, 'r')
 		org_data = hfile.read()
 		hfile.close()
 		if -1 == org_data.find('PROTOCOL_API int get_version();'):
-			org_data = org_data.replace('#undef PROTOCOL_API',str_h_getversion)
-		hfile = open(path + str_hfile, 'w')
+			org_data = org_data.replace('#undef PROTOCOL_API', str_h_getversion)
+		hfile = open(root + str_hfile, 'w')
 		hfile.write(org_data)
 		hfile.close()
-	except IOError,e:
-		return [1,e]
-	return [0,0]
+	except IOError, e:
+		return [1, e]
+	return [0, 0]
 
 
+def change_to_ios_staticlib(root, prefix, global_list, static_list, function_list):
+	function_list.append('send_cmd')
+	function_list.append('receive_cmd')
+	function_list.append('time_delay_ms')
+	function_list.append('calculate')
+	global_list.append('ReturnStatus')
 
+	# print path, prefix, globalData, staticData, functionData
+	# return 0
 
-def change_to_IOS_StaticLib(path, prefix, globalData, staticData, functionData):
-	functionData.append('send_cmd')
-	functionData.append('receive_cmd')
-	functionData.append('time_delay_ms')
-	functionData.append('calculate')
-	globalData.append('ReturnStatus')
+	allnewfiles = find_file_by_types(root, True, 'c', 'h')
 
-	#print path, prefix, globalData, staticData, functionData
-	#return 0
-
-	allnewfiles = findFileByTypes(path, True, 'c', 'h')
-
-	result = find_all_C_functions(allnewfiles)
-	functionData = functionData + result[1]
+	list_allfunctions = find_all_c_functions(allnewfiles)
+	function_list = function_list + list_allfunctions[1]
 
 	# 删除相同的前缀
-	functionData = remove_the_same_prefix(functionData)
-	globalData = remove_the_same_prefix(globalData)
+	function_list = remove_the_same_prefix(function_list)
+	global_list = remove_the_same_prefix(global_list)
 
 	for newfile in allnewfiles:
 		try:
@@ -206,7 +205,7 @@ def change_to_IOS_StaticLib(path, prefix, globalData, staticData, functionData):
 			data_org = c_file.readlines()
 			c_file.close()
 		except IOError, e:
-			return [1,e]
+			return [1, e]
 		else:
 			data_new = ''
 			for line in data_org:
@@ -214,23 +213,22 @@ def change_to_IOS_StaticLib(path, prefix, globalData, staticData, functionData):
 				if '#include' not in line:
 
 					# 增加程序释放动态分配内存后,指针没有赋值为NULL的情况
-					line = add_NULL_For_Free(newfile,line)
+					line = add_null_for_free(newfile, line)
 
 					# 处理静态变量
-					for item in staticData:
+					for item in static_list:
 						if (item[1] in line) and (item[0] in newfile):
 							line = 'static ' + line
 
 					# 处理全局变量和函数名的时候,会出现重复处理的问题,如g_p_stIdleLinkConfig和g_p_stIdleLinkConfigGroup,会执行两次替换
-					for item in functionData:
+					for item in function_list:
 						if item in line:
 							line = line.replace(item, prefix + item)
 
 					# 同一个位置仅替换一次是否可行,这样从长的字符串开始替换,就不会出错了吧?但怎么解决查找多个字符串的问题?
-					for item in globalData:
+					for item in global_list:
 						if item in line:
 							line = line.replace(item, prefix+item)
-
 
 				data_new = data_new + line
 				try:
@@ -239,15 +237,15 @@ def change_to_IOS_StaticLib(path, prefix, globalData, staticData, functionData):
 					c_file.close()
 				except IOError, e:
 					return [1, e]
-	return [0,0]
+	return [0, 0]
 
 
 if __name__ == '__main__':
 
 	str_CopyProjectNametxt = 'CopyProjectName.txt'
 	str_ProtocolFile = 'PROTOCOL'
-	path = os.getcwd() # 取得当前目录路径
-	pathCopyProjectName = os.path.join(path,str_CopyProjectNametxt)  # 合成全路径
+	path = os.getcwd()  # 取得当前目录路径
+	pathCopyProjectName = os.path.join(path, str_CopyProjectNametxt)  # 合成全路径
 	try:
 		projectfile = open(pathCopyProjectName, 'r')
 		str_projectName = projectfile.readlines()
@@ -256,11 +254,11 @@ if __name__ == '__main__':
 	except IOError, e:
 		str_prefix = raw_input('input name: ')
 
-	pathPROTOCOL = os.path.join(path,str_ProtocolFile)  # 合成全路径
-	if False == os.path.isdir(pathPROTOCOL):  # 判断文件夹是否存在
+	pathPROTOCOL = os.path.join(path, str_ProtocolFile)  # 合成全路径
+	if not os.path.isdir(pathPROTOCOL):  # 判断文件夹是否存在
 		str_fail = 'Can not find %s\n' % pathPROTOCOL
 		result = [1, str_fail]
-	else :
+	else:
 		result = change_init(path)
 		if 1 == result[0]:
 			print result[1]
@@ -270,21 +268,19 @@ if __name__ == '__main__':
 			functionData = result[3]
 
 			print 'running...'
-			pathNewPROTOCOL = os.path.join(path,str_prefix)
-			if True == os.path.isdir(pathNewPROTOCOL): # 如果存在该目录,则先删除
+			pathNewPROTOCOL = os.path.join(path, str_prefix)
+			if os.path.isdir(pathNewPROTOCOL):  # 如果存在该目录,则先删除
 				shutil.rmtree(pathNewPROTOCOL)
-			shutil.copytree(pathPROTOCOL, pathNewPROTOCOL) #copy一份
+			shutil.copytree(pathPROTOCOL, pathNewPROTOCOL)  # copy一份
 
 			result = add_getversion_api(pathNewPROTOCOL)
 			if 1 == result[0]:
 				print result[1]
 				raw_input('Enterkey to exit')
 			else:
-				result = change_to_IOS_StaticLib(pathNewPROTOCOL, str_prefix+'_', globalData, staticData, functionData)
+				result = change_to_ios_staticlib(pathNewPROTOCOL, str_prefix + '_', globalData, staticData, functionData)
 				if 1 == result[0]:
 					print result[1]
 					raw_input('Enterkey to exit')
 				else:
 					print 'program finish'
-
-
